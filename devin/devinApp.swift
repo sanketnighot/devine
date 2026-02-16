e8f684fc3d57fd1788792559c86b9820449be7cb
Sanket Nighot <sanketnighot25@gmail.com>
2026-02-16 14:57:16 +0530

    Initial Commit

---
+++ devin/devinApp.swift
@@ -0,0 +1,17 @@
+//
+//  devinApp.swift
+//  devin
+//
+//  Created by Sanket Nighot on 16/02/26.
+//
+
+import SwiftUI
+
+@main
+struct devinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
